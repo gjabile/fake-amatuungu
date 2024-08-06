7fd1739cff752253165c6ba0c2b17cbde38c27a0
gjabile <glpjabile@gmail.com>
2024-08-06 23:54:45 +0300

    made changes for vercel

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.get('/', (req, res) => {
   res.send('Hello, Vercel!');
 });
 
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
 
 module.exports = app;
